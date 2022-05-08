4047799e9c519e41cba5e7ea9bf139b6dba64715
TubagusHamzahAchyari <achyarimailbox@gmail.com>
2022-05-08 07:47:19 +0700

    Update

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 """
-ini adalah demo project pertama dengan Python
+Semua sintaksis dasar bahasa pemrograman terdiri dari:
+1. Sekuensial: langkah berurutan
+2. Percabangan:
+3.
 """
-print("Heloo world!")
-print("Heloo world!")
+print('Ibu berkata "Budi pergi ke toko"')
+print('Budi berkata "Baik Ibu, apa yang harus saya lakukan di toko"')
+print('Ibu berkata "Beli satu botol susu, dan jika ada telur belikan 6 telur juga"')
+print('Maka Budi berangkat ke toko')
+print('Budi kembali pulang')
+print('Budi memberikan hasil dari toko kepada Ibu')
+
